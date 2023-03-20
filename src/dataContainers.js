30510316f2e907bbb3b430095373c0ea6da05bdd
alexVera99 <alexanderjoel.vera99@gmail.com>
2023-03-20 22:20:14 +0100

    update: position belongs to user instead of scenenode

--- src/dataContainers.js
+++ src/dataContainers.js
@@ -60,7 +60,6 @@ export class SceneNode {
     material;
     scale;
     animations;
-    position;
 
     constructor() {
         this.animations = new Object();
@@ -74,7 +73,6 @@ export class SceneNode {
         
         this.scale = data["scale"];
         this.parseAnimationFromJSON(data["animations"])
-        this.position = data["position"];
     }
 
     addAnimation(name, anim){
@@ -102,6 +100,7 @@ export class User {
         this.username = username || "";
         this.room_id = null; //room name
         this.scene_node;
+        this.position = [0.0, 0.0, 0.0];
     }
 
     fromJSON(data, isCurrenUser = false) {
@@ -111,14 +110,15 @@ export class User {
         this.room_id = data["room_id"];
         this.scene_node = new SceneNode();
         this.scene_node.fromJSON(data["scene_node"]);
+        this.position = data["position"];
     }
 
     updatePosition(position) {
-        this.scene_node.position = position;
+        this.position = position;
     }
 
     getPosition() {
-        return this.scene_node.position;
+        return this.position;
     }
 }
 
